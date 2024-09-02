c263e68fe58150df837a4af6b3f0775dd841958a
kavinesh <kavineshkavinesh860@gmail.com>
2024-09-02 22:30:29 +0530

    common view js file

---
+++ Flixmate_Project/src/js/views/view.js
@@ -0,0 +1,13 @@
+export default class View {
+  _data;
+  render(data) {
+    this._data = data;
+    // this.#clear();
+    const markup = this._generateMarkup();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  #clear() {
+    this._parentElement.innerHTML = "";
+  }
+}
