7448e90cabbd54cfeed8f2658913f7859cb08e08
kavinesh <kavineshkavinesh860@gmail.com>
2024-09-09 15:11:39 +0530

    javascript file

--- Lets-build--Project/src/js/script.js
+++ Lets-build--Project/src/js/script.js
@@ -1,45 +1,24 @@
-const projectContainer = document.querySelector(".section-projects");
-
 // fetch("https://neon-crumble-ae55e3.netlify.app/.netlify/functions/videos")
 //   .then((response) => response.json())
 //   .then((data) => console.log(data))
 //   .catch((error) => console.error("Error:", error));
 
-const loadProject = async function () {
-  const res = await fetch(
-    "https://66d85b0a8e2116b49ed4354e--lets-build-api.netlify.app/api/videos"
-  );
+// const loadProject = async function () {
+//   const res = await fetch(
+//     "https://66d9e7d1395ede3bb29d95ba--hackersnotdie.netlify.app/.netlify/functions/videos"
+//   );
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  if (!res.ok) console.error("nOt working");
-  console.log(data);
-};
+//   if (!res.ok) console.error("nOt working");
+//   console.log(data);
+// };
 
-loadProject();
+// loadProject();
 
-fetch("https://66d85b0a8e2116b49ed4354e--lets-build-api.netlify.app/api/videos")
-  .then((res) => res.json())
-  .then(console.log)
-  .catch(console.error);
+// const buttonEl = document.querySelector(".btn--mobile-nav");
+// const headerEl = document.querySelector(".header");
 
-// document
-//   .querySelector(".project-card")
-//   .addEventListener("mouseenter", function () {
-//     console.log("entered");
-//     document.querySelector(".play-icon").style.display = "block";
-//   });
-
-// document
-//   .querySelector(".project-card")
-//   .addEventListener("mouseout", function () {
-//     console.log("exit");
-//     document.querySelector(".play-icon").style.display = "none";
-//   });
-
-const buttonEl = document.querySelector(".btn--mobile-nav");
-const headerEl = document.querySelector(".header");
-
-buttonEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+// buttonEl.addEventListener("click", function () {
+//   headerEl.classList.toggle("nav-open");
+// });
